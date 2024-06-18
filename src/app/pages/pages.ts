0bef74f2b8e1bacd201a93376a355c9b437c4d7e
unknown <sparklingice099>
2024-06-18 16:17:58 +0800

    chore: asdjustments

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -2,6 +2,5 @@ export const pages = [
   { index: 0, name: 'overview.md', route: '/overview', visible: true },
   { index: 1, name: 'skills.md', route: '/skills', visible: true },
   { index: 2, name: 'experience.md', route: '/experience', visible: true },
-  { index: 3, name: 'education.md', route: '/education', visible: true },
-  { index: 4, name: 'projects.md', route: '/projects', visible: true },
+  { index: 3, name: 'projects.md', route: '/projects', visible: true },
 ];
